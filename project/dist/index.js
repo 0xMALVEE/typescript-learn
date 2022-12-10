adfb762952ce5eb4b5d43d26366bf0e7d269e5c8
Alvee <35369563+0xMALVEE@users.noreply.github.com>
2022-12-10 17:32:23 +0600

    some code

--- project/dist/index.js
+++ project/dist/index.js
@@ -1,8 +1,26 @@
 "use strict";
+// class User{
+//     email: string
+//     name: string
+//     constructor(name: string,email: string){
+//         this.name = name;
+//         this.email = email;
+//     }
+// }
+// const jhon = new User("jhon", "jhon@")
 class User {
-    constructor(name, email) {
+    constructor(name, email, key) {
         this.name = name;
         this.email = email;
+        this.key = 34;
+        this.key = key;
+    }
+    get getKey() {
+        return this.key;
+    }
+    set setKey(num) {
+        this.key = num;
     }
 }
-const jhon = new User("jhon", "jhon@");
+const jhon = new User("jhon", "jhon@", 33);
+console.log(jhon.getKey);
--- project/src/index.ts
+++ project/src/index.ts
@@ -1,10 +1,29 @@
+// class User{
+//     email: string
+//     name: string
+//     constructor(name: string,email: string){
+//         this.name = name;
+//         this.email = email;
+//     }
+// }
+
+// const jhon = new User("jhon", "jhon@")
+
+
 class User{
-    email: string
-    name: string
-    constructor(name: string,email: string){
-        this.name = name;
-        this.email = email;
+   private key: number = 34
+    constructor(public name: string,public email: string, key: number){
+        this.key = key
+    }
+    get getKey(){
+        return this.key
+    }
+    set setKey(num: number){
+        this.key = num
     }
 }
 
-const jhon = new User("jhon", "jhon@")
+const jhon = new User("jhon", "jhon@", 33)
+
+console.log(jhon.getKey)
+
